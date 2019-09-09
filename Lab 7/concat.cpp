241fac5c2eda2ceb6256310db64ed8f26898964c
bg2404 <bhasker2000goel@gmail.com>
2019-09-09 14:44:25 +0530

    Lab 7

---
+++ Lab 7/concat.cpp
@@ -0,0 +1,41 @@
+#include <iostream> 
+#include <string> 
+#include <vector> 
+#include <algorithm> 
+using namespace std; 
+  
+int myCompare(string X, string Y) 
+{ 
+    string XY = X.append(Y); 
+  
+    string YX = Y.append(X); 
+  
+    return XY.compare(YX) > 0 ? 1: 0; 
+} 
+  
+void printLargest(vector<string> arr) 
+{ 
+    sort(arr.begin(), arr.end(), myCompare); 
+  
+    for (int i=0; i < arr.size() ; i++ ) 
+        cout << arr[i]; 
+} 
+  
+int main() 
+{ 
+    int t;
+    cin >> t;
+    while(t--) {
+        vector<string> arr; 
+        int n;
+        cin >> n;
+        string s;
+        for(int i = 0; i < n; ++i) {
+            cin >> s;
+            arr.push_back(s);
+        } 
+        printLargest(arr);
+        cout << '\n'; 
+    }
+   return 0; 
+} 
